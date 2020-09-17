8ed2f5d0f25f2cc23e02e97ebdf86b7709ceb034
Terry Alberto Vargas Santa Cruz <terry.vargas@unas.edu.pe>
2020-09-17 13:08:11 -0500

    Agregar archivos de proyecto.

---
+++ WebApplication3/Controllers/PersonaController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class PersonaController : Controller
+    {
+        // GET: PersonaController
+
+        Context ContextoPersona = new Context();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: PersonaController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: PersonaController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PersonaController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Persona model)
+        {
+            ContextoPersona.Persona.Add(model);
+            ContextoPersona.SaveChanges();
+            return View();
+        }
+
+        // GET: PersonaController/Edit/5
+        public ActionResult Edit(int id)
+        {
+          Persona  MiPersona = ContextoPersona.Persona.Find(id);
+            return View(MiPersona);
+        }
+
+        // POST: PersonaController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Persona model)
+        {
+
+
+
+
+            ContextoPersona.Persona.Attach(model);
+            ContextoPersona.Entry(model).State = EntityState.Modified;
+            ContextoPersona.SaveChanges();
+
+            ContextoPersona.SaveChanges();
+            return RedirectToAction("List");
+        }
+
+        // GET: PersonaController/Delete/5
+        public ActionResult List()
+        {
+            IEnumerable<Persona> ListaPersonas = ContextoPersona.Persona.ToList();
+            return View(ListaPersonas); 
+        }
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            Persona MiPersona = ContextoPersona.Persona.Find(id);
+       
+                return View(MiPersona);
+            
+        }
+        // POST: PersonaController/Delete/5
+        [HttpPost]
+
+        public ActionResult Delete(Persona model)
+        {
+
+            Persona MiPersona = ContextoPersona.Persona.Find(model.id);
+            ContextoPersona.Persona.Remove(MiPersona);
+            ContextoPersona.SaveChanges();
+
+            return View();
+        }
+    }
+}
---
+++ WebApplication3/Models/Context.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication3.Models
+{
+    public class Context : DbContext
+    {
+        public Context() : base("Server=LAPTOP-2GGF1CMS;Database=Persistence;User Id=terry;Password=123;") { }
+
+
+        public DbSet<Persona> Persona { get; set; }
+    }
+}
